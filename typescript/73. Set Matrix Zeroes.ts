e4e026fe162e85cfc0691d98d40fdab67a6521c3
Anton Khorn <anton.pres@yandex.ru>
2024-11-07 22:51:41 +0300

    73

--- typescript/73. Set Matrix Zeroes.ts
+++ typescript/73. Set Matrix Zeroes.ts
@@ -1,60 +1,63 @@
-function printMatrix(matrix) {
+function printMatrix(matrix: number[][]): void {
     for (let i = 0; i < matrix.length; i++) {
         const row = matrix[i];
         console.log(row);
     }
 }
-function setUndef(matrix, i, j) {
+
+function setUndef(matrix: number[][], i: number, j: number): void{
     matrix[i] = Array(matrix[0].length).fill(0);
+
     for (let x = 0; x < matrix.length; x++) {
         matrix[x][j] = 0;
     }
 }
-function recursiveFill(matrix, is, js, isInit = false) {
-    let i = -1, j = -1;
+
+function recursiveFill(matrix: number[][], is: number, js: number, isInit: boolean = false): void{
+    let i: number = -1, j: number = -1;
     let element = -1;
     for (i = is; i < matrix.length; i++) {
         for (j = js; j < matrix[0].length; j++) {
-            if (i == is && j == js && !isInit)
-                continue;
+            if (i == is && j == js && !isInit) continue;
             element = matrix[i][j];
-            if (element == 0) {
+            if (element == 0){
                 recursiveFill(matrix, i, j);
                 break;
             }
         }
-        js = 0;
-        if (element == 0)
-            break;
+        js=0;
+        if (element == 0) break;
     }
-    if (element == 0)
-        setUndef(matrix, i, j);
+    if (element == 0) setUndef(matrix, i, j);
 }
 /**
  Do not return anything, modify matrix in-place instead.
  */
-function setZeroes(matrix) {
+ function setZeroes(matrix: number[][]): void {
     recursiveFill(matrix, 0, 0, true);
-}
-;
-let matrix = [[1, 1, 1], [1, 0, 1], [1, 1, 1]];
+ };
+
+
+let matrix = [[1,1,1],[1,0,1],[1,1,1]];
 setZeroes(matrix);
 printMatrix(matrix);
 console.log("=============");
-printMatrix([[1, 0, 1], [0, 0, 0], [1, 0, 1]]);
+printMatrix([[1,0,1],[0,0,0],[1,0,1]]);
 console.log("");
-matrix = [[0, 1, 2, 0], [3, 4, 5, 2], [1, 3, 1, 5]];
+
+matrix = [[0,1,2,0],[3,4,5,2],[1,3,1,5]];
 printMatrix(matrix);
 console.log("=============");
 setZeroes(matrix);
 printMatrix(matrix);
 console.log("=============");
-printMatrix([[0, 0, 0, 0], [0, 4, 5, 0], [0, 3, 1, 0]]);
+printMatrix([[0,0,0,0],[0,4,5,0],[0,3,1,0]]);
 console.log("");
-matrix = [[1, 2, 3, 4], [5, 0, 7, 8], [0, 10, 11, 12], [13, 14, 15, 0]];
+
+matrix = [[1,2,3,4],[5,0,7,8],[0,10,11,12],[13,14,15,0]];
 printMatrix(matrix);
 console.log("=============");
 setZeroes(matrix);
 printMatrix(matrix);
 console.log("=============");
-printMatrix([[0, 0, 3, 0], [0, 0, 0, 0], [0, 0, 0, 0], [0, 0, 0, 0]]);
+printMatrix([[0,0,3,0],[0,0,0,0],[0,0,0,0],[0,0,0,0]]);
--- typescript/run_ts.ps1
+++ typescript/run_ts.ps1
@@ -1,4 +1,4 @@
-$file_name = "48. Rotate Image"
+$file_name = "73. Set Matrix Zeroes"
 
 tsc ($file_name + ".ts") --target esnext --outfile index.js
 
